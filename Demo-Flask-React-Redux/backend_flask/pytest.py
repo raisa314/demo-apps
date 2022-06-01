49beebcd5b4fb80ee9129c8714b13dc7881aa3c4
Jf.Raisa <pi314raisa@gmail.com>
2022-06-01 13:29:41 +0600

    pytest file updated

--- Demo-Flask-React-Redux/backend_flask/pytest.py
+++ Demo-Flask-React-Redux/backend_flask/pytest.py
@@ -1,15 +1,15 @@
-# from app import test_meeting
-# def app():
-#     user = test_meeting()
-#     return user
-# app()
+from app import test_meeting
+def app():
+    user = test_meeting()
+    return user
+app()
 
 # import requests
-import subprocess
-from app import test_meeting
+# import subprocess
+# from app import test_meeting
 
-os_name = subprocess.run(["lsb_release", "-a"])
-print("The exit code was: %d" % os_name.returncode)
+# os_name = subprocess.run(["lsb_release", "-a"])
+# print("The exit code was: %d" % os_name.returncode)
 
 # def app():
 #     print("hi success")
